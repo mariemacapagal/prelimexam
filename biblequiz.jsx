784bae537eca695a0b6107ff7f54bb1507c072b0
Marie Macapagal <emariemacapagal@gmail.com>
2024-01-30 21:36:24 +0800

    Add files via upload

--- biblequiz.jsx
+++ biblequiz.jsx
@@ -3,7 +3,7 @@ function True() {
         <div>
             <h3>The Bible consists of the Old and New Testaments.</h3>
             <h4 className="correct">Correct!</h4>
-            <h3>Mosses led the Israelites out of Egypt.hehe</h3>
+            <h3>Mosses led the Israelites out of Egypt.hehffe</h3>
             <h4 className="correct">Correct!</h4>
             <h3>The Sermon on the Mount is found in the Book of Luke.</h3>
             <h4 className="incorrect">Incorrect!</h4>

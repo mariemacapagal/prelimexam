5bd8ec888c10605debbd41bf4fd38f98e8bebab4
Marie Macapagal <emariemacapagal@gmail.com>
2024-01-30 15:29:14 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+// Ed Marie May Macapagal
+
+import Result from './biblequiz'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>The Bible Quiz</h1>
+        <Result />
+      </div>
+    </>
+  )
+}
+
+export default App()
